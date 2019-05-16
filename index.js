3ce3c9ddee2bc50d1df686d08e8985952592132c
DeepalSuranga <deepal.suranga29@gmail.com>
2019-05-16 14:19:33 +0530

    with not run

--- index.js
+++ index.js
@@ -10,62 +10,71 @@ var t_current_minute = date.getMinutes();
 var t_current_seconds = date.getSeconds();
 var t_current_miliseconds = date.getMilliseconds();
 
+var bt1_i_gen_id = 0;
+var bt1_s_name ='Deepal';
+var bt1_i_runs = 0;
+var bt1_i_balls = 0;
+var bt1_i_fours = 0;
+var bt1_i_sixs = 0;
+var bt1_i_strike_rate = bt1_i_runs / bt1_i_balls;
+
+var bt2_i_gen_id = 0;
+var bt2_s_name ='Malinda';
+var bt2_i_runs = 0;
+var bt2_i_balls = 0;
+var bt2_i_fours = 0;
+var bt2_i_sixs = 0;
+var bt2_i_strike_rate = bt2_i_runs / bt2_i_balls;
+
+
 var b_i_runs = 0;
 var b_i_overs = 0;
 
 var b_i_economy_rate = b_i_runs / b_i_overs;
 
-var bt1_i = runs1;
-var bt1_i = balls1;
-var bt1_i_strike_rate = runs1 / balls1;
-
-var bt2_i = runs2;
-var bt2_i = balls2;
-var bt2_i_strike_rate = runs2 / balls2;
-
 const current_scores = [];
 
 const current_score = [
     {
-        batting1: [
+        op_batting1: [
             {
-                id: 1,
-                batsman: 'deepal',
-                runs: runs1,
-                balls: balls1,
-                fours: 1,
-                sixs: 0,
-                strike_rate: bt1_i_strike_rate,
-                description: [
+                vo_id: bt1_i_gen_id,
+                vo_name: bt1_s_name,
+                vo_runs: bt1_i_runs,
+                vo_balls: bt1_i_balls,
+                vo_fours: bt1_i_fours,
+                vo_sixs: bt1_i_sixs,
+                vo_strike_rate: bt1_i_strike_rate,
+                vo_description: [
                     {
-                        current_status: 'out',
-                        current_description: 'c Malinda b Deepal',
+                        vo_current_status: 'out',
+                        vo_current_description: 'c Malinda b Deepal',
 
                     }
                 ]
 
             }
         ],
-        batting2: [
+        op_batting2: [
             {
-                id: 1,
-                batsman: 'malinda',
-                batsman1_runs: 1,
-                balls: runs2,
-                fours: balls2,
-                sixs: 0,
-                strike_rate: bt2_i_strike_rate,
-                description: [
+                vs_id: bt2_i_gen_id,
+                vs_name: bt2_s_name,
+                vs_runs: bt2_i_runs,
+                vs_balls: bt2_i_balls,
+                vs_fours: bt2_i_fours,
+                vs_sixs: bt2_i_sixs,
+                vs_strike_rate: bt2_i_strike_rate,
+                vs_description: [
                     {
-                        current_status: 'not out',
-                        current_description: 'currently batting',
+                        vs_current_status: 'not out',
+                        vs_current_description: 'currently batting',
 
                     }
                 ]
 
             }
         ],
-        bowling: [
+        op_bowling: [
             {
                 id: 1,
                 bowller: 'supun',
@@ -79,29 +88,33 @@ const current_score = [
         ],
         final_score: [
             {
-                id: 1,
-                batting_team: team1,
-                total_score: 42,
-                total_wkts: 2,
-                total_overs: 4.5,
-                extras: [
+                f_id: 1,
+                f_batting_team: 'team1',
+                f_total_score: 42,
+                f_total_wkts: 2,
+                f_total_overs: 4.5,
+                f_extras: [
                     {
-                        wide_balls: 0,
-                        no_balls: 0,
-                        leg_byes: 0,
-                        byes: 0,
+                        f_wide_balls: 0,
+                        f_no_balls: 0,
+                        f_leg_byes: 0,
+                        f_byes: 0,
                     }
                 ]
 
             }
         ],
-        host_time:t_current_hour+":"+t_current_minute+":"+t_current_seconds+":"+t_current_miliseconds
+        host_time: t_current_hour + ":" + t_current_minute + ":" + t_current_seconds + ":" + t_current_miliseconds
 
     }
 ];
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
 
     res.send("WELCOME CRICKET APPLICATION");
 
 });
+
+// server config
+const port = process.env.PORT || 3000;
+app.listen(port,() => console.log(`Listening on port ${port}`));
